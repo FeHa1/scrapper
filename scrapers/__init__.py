3ea0443107946cebd12add4566fde9cd6ae7ff00
FeHa1 <fedehayes2001@gmail.com>
2026-04-13 11:21:10 -0300

    orden de carpetas

---
+++ scrapers/__init__.py
@@ -0,0 +1,3 @@
+from .outscraper_maps import scrape_google_maps
+from .superprof import scrape_superprof
+from .tusclases import scrape_tusclases
---
+++ utils/__init__.py
@@ -0,0 +1,13 @@
+from .helpers import (
+    build_record,
+    deduplicate,
+    extract_email,
+    extract_instagram,
+    has_contact,
+    load_existing_csv,
+    normalize_phone,
+    normalize_text,
+    safe_get,
+    save_to_csv,
+    setup_logging,
+)
